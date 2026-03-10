ac556d889a34cb0f6b6966aedcf959143ccc41e4
Abhilash Tripathy <abhilashtripathy25@gmail.com>
2026-03-10 20:16:23 +0530

    day 1

---
+++ basics/day1.py
@@ -0,0 +1,62 @@
+# print("my first day of learning DSA")
+# a =5
+# b=2
+# num = 10
+# sum = a+b
+# print(sum)
+# #arithmetic operators
+# print(a+b)
+# print(a/b)
+# print(a-b)
+# print(a*b)
+# print(a%b)
+# print(a**b)
+# #relational operators
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a>=b)
+# print(a<=b)
+# #assignment operators
+# # num =  num +10
+# # num += 10
+# # num -= 10
+# # num /= 5
+# # num *= 10
+# # num %= 3
+# num **= 2
+# print("num :", num )
+
+# #logical operators
+# print(not False)
+# print(not (a>b))
+# val1 = True
+# val2 = True
+# print("and operator: ", val1 and val2)
+# print("and operator: ", val1 or val2)
+# #type casting
+
+# c = int("7")
+# d = 9
+# print(c +d )
+# #input 
+# # name = input("Enter your name : ")
+# # print("welcome ", name)
+# name = input("Enter your name :")
+# age = int(input("Enter your Age :"))
+# marks = float(input("Enter your marks :"))
+# print (name)
+# print (age)
+# print (marks)
+
+# num1 = int(input("enter first num :"))
+# num2 = int(input("enter second num :"))
+
+# print("sum of num1 and num2 is :", num1 + num2)
+
+# side = int(input("enter the side of the squre :"))
+# print("area of the Squre is ", side * side)
+
+# print("the average of the entered nums is ", (num1 + num2)/ 2)
+# print(num1 >= num2)
